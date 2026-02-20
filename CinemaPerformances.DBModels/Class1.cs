6a9bbcb4cfe2e36251d99a9f3494a07f63e937ad
uwuhasmile <uwuhasmile@proton.me>
2026-02-21 00:15:59 +0200

    DBModels: Performance model definition

--- CinemaPerformances.DBModels/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CinemaPerformances.DBModels;
-
-public class Class1
-{
-
-}
---
+++ CinemaPerformances.DBModels/PerformanceDBModel.cs
@@ -0,0 +1,30 @@
+using CinemaPerformances.Common;
+
+namespace CinemaPerformances.DBModels;
+
+public class PerformanceDBModel
+{
+    public readonly Guid Id;
+    public readonly Guid CinemaHallId;
+    public string Name { get; set; }
+    public MovieGenre Genre { get; set; }
+    public readonly DateTime ReleaseDate;
+    public DateTime Start { get; set; }
+    public double Duration { get; set; }
+    public DateTime EndTime => Start.AddMinutes(Duration);
+
+    private PerformanceDBModel()
+    {
+    }
+
+    public PerformanceDBModel(Guid cinemaHallId, string name, MovieGenre genre, DateTime releaseDate, DateTime start, double duration)
+    {
+        Id = Guid.NewGuid();
+        CinemaHallId = cinemaHallId;
+        Name = name;
+        Genre = genre;
+        ReleaseDate = releaseDate;
+        Start = start;
+        Duration = duration;
+    }
+}
