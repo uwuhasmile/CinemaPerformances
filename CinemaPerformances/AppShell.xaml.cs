3a2aa718dd058cf50560bbb9db7fde3bfaeb300c
uwuhasmile <uwuhasmile@proton.me>
2026-03-04 12:38:14 +0200

    MAUI project setup

---
+++ CinemaPerformances/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaPerformances;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
--- CinemaPerformances/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CinemaPerformances.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
