c2089a79a82998f96c059a3788fe67d29c9ec494
uwuhasmile <uwuhasmile@proton.me>
2026-03-06 20:29:36 +0200

    Enum extension for retrieving the display name

---
+++ CinemaPerformances.Common/EnumExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace CinemaPerformances.Common;
+
+public sealed record EnumWithName<TEnum>(TEnum Value, string DisplayName) where TEnum : struct, Enum;
+public static class EnumExtensions
+{
+    public static string GetDisplayName(this Enum value)
+    {
+        var type = value.GetType();
+        var name = Enum.GetName(type, value);
+        if (name is null)
+            return value.ToString();
+
+        var field = type.GetField(name);
+        var display = field?.GetCustomAttribute<DisplayNameAttribute>();
+        return display?.DisplayName ?? name;
+    }
+
+    public static EnumWithName<TEnum> GetEnumWithName<TEnum>(this TEnum value) where TEnum : struct, Enum
+    {
+        return new EnumWithName<TEnum>(value, value.GetDisplayName());
+    }
+
+    public static EnumWithName<TEnum>[] GetValueWithNames<TEnum>() where TEnum : struct, Enum
+    {
+        var values = Enum.GetValues<TEnum>();
+        var result = new EnumWithName<TEnum>[values.Length];
+        for (int i = 0; i < values.Length; i++)
+        {
+            result[i] = values[i].GetEnumWithName();
+        }
+        return result;
+    }
+}
