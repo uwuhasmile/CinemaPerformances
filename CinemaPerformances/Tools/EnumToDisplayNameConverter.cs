e328a9c2ba05c0c72e3933e168f75d7908da63b8
uwuhasmile <uwuhasmile@proton.me>
2026-03-06 20:36:27 +0200

    MAUI enum converter

---
+++ CinemaPerformances/Tools/EnumToDisplayNameConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using CinemaPerformances.Common;
+
+namespace CinemaPerformances.Tools;
+
+public class EnumToDisplayNameConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is null)
+            return string.Empty;
+
+        if (value is not Enum enumeration)
+            return value.ToString() ?? string.Empty;
+
+        return enumeration.GetDisplayName();
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
